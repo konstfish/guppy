2edf279d701dd74d9676f7a4c0171849ddf8edbf
David Fischer <david@konst.fish>
2024-04-14 18:35:10 +0200

    feat(harbor): finish up config

--- terraform/harbor.tf
+++ terraform/harbor.tf
@@ -5,31 +5,6 @@ resource "helm_release" "harbor" {
   namespace        = "harbor"
   create_namespace = true
 
-  set {
-    name  = "expose.ingress.hosts.core"
-    value = "harbor.konst.fish"
-  }
-
-  set {
-    name  = "expose.tls.secret.secretName"
-    value = "harbor-tls"
-  }
-
-  set {
-    name  = "expose.ingress.harbor.annotations.external-dns\\.alpha\\.kubernetes\\.io/hostname"
-    value = "harbor.konst.fish"
-  }
-
-  set {
-    name  = "externalURL"
-    value = "https://harbor.konst.fish"
-  }
-
-  set {
-    name  = "persistence.imageChartStorage.filesystem.rootdirectory"
-    value = "/stor/harbor"
-  }
-
   values = [
     file("${path.module}/cluster/helm/harbor/values.yml")
   ]
